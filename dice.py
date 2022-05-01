1d5920b9127295f2c33fc80f1c8c1855dce5d126
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-05-01 11:24:43 -0300

    Update dice.py

--- dice.py
+++ dice.py
@@ -26,7 +26,7 @@ def roll(n:int,d:int):
         print("Heads")
     elif d == 2 and total == 2:
         print("Tails")
-    print("Dice:",total)
+    print("Dice result:",total)
     return total
 
 def main():
