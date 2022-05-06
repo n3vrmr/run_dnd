14800c32a9c79dfc64a7aa4c37a9142523c479ec
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-05-05 22:34:01 -0300

    Add files via upload

---
+++ 4d6_k3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 22:09:23 2022
+
+@author: Nevermore
+"""
+
+import dice as d
+
+def abilitiy_rolls():
+    """Rolls 4d6 and adds the three highest values.
+    Useful for rolling values for your character's ability scores."""
+    d6 = d.roll(4,6,True)
+    del d6[0]
+    adds = sum(d6)
+    print(adds)
+    return adds
+
+def main():
+    print("Randomize")
+    
+if __name__ == '__main__':
+    main()
+    abilitiy_rolls()
