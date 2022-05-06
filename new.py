e4ba5f8c89f2a834f2f6c85483af5c7b1d66bcfa
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-05-05 22:04:57 -0300

    Add files via upload

--- new.py
+++ new.py
@@ -6,46 +6,59 @@ Created on Wed May  4 18:45:16 2022
 """
 
 import dice as d
+import time as t
 
 class Character:
-    def __init__(self, Name, Level,
-                 ability_scores, ability_mods,
-                 proficiency_bonus, death_saves):
-        self.Name = input("Character name: ")
-        self.Level = input("Character level: ")
+    def __init__(self, name, level:int,
+                 ability_scores={}, ability_mods={},
+                 proficiency_bonus=0):
+        self.name = name
+        self.level = level
+        return
+        
+    def abilities(self):        
+        print("Enter your ability scores:")
+        t.sleep(1.2)
         self.ability_scores = {}
         score = True
         while score:
             score = False
-            ability_scores["Strength"] = int(input("Strength: "))
-            ability_scores["Dexterity"] = int(input("Dexterity: "))
-            ability_scores["Constitution"] = int(input("Constitution: "))
-            ability_scores["Intelligence"] = int(input("Intelligence: "))
-            ability_scores["Wisdom"] = int(input("Wisdom: "))
-            ability_scores["Charisma"] = int(input("Charisma: "))
+            self.ability_scores["Strength"] = int(input("Strength: "))
+            self.ability_scores["Dexterity"] = int(input("Dexterity: "))
+            self.ability_scores["Constitution"] = int(input("Constitution: "))
+            self.ability_scores["Intelligence"] = int(input("Intelligence: "))
+            self.ability_scores["Wisdom"] = int(input("Wisdom: "))
+            self.ability_scores["Charisma"] = int(input("Charisma: "))
+    
         self.ability_mods = {}
         modifier = True
         while modifier:
             modifier = False
-            ability_mods["Strength"] = (ability_scores.get("Strength") - 10)//2
-            ability_mods["Dexterity"] = (ability_scores.get("Dexterity") - 10)//2
-            ability_mods["Constitution"] = (ability_scores.get("Constitution") - 10)//2
-            ability_mods["Intelligence"] = (ability_scores.get("Intelligence") - 10)//2
-            ability_mods["Wisdom"] = (ability_scores.get("Wisdom") - 10)//2
-            ability_mods["Charisma"] = (ability_scores.get("Charisma") - 10)//2
-        if self.Level <=4:
+            self.ability_mods["Strength"] = (self.ability_scores.get("Strength") - 10)//2
+            self.ability_mods["Dexterity"] = (self.ability_scores.get("Dexterity") - 10)//2
+            self.ability_mods["Constitution"] = (self.ability_scores.get("Constitution") - 10)//2
+            self.ability_mods["Intelligence"] = (self.ability_scores.get("Intelligence") - 10)//2
+            self.ability_mods["Wisdom"] = (self.ability_scores.get("Wisdom") - 10)//2
+            self.ability_mods["Charisma"] = (self.ability_scores.get("Charisma") - 10)//2
+        return
+    
+    def proficiency(self):
+        if self.level <=4:
             pb = 2
             return pb
-        elif self.Level >=5 and self.Level <=8:
+        elif self.level >=5 and self.level <=8:
             pb = 3
             return pb
-        elif self.Level >=9 and self.Level <=12:
+        elif self.level >=9 and self.level <=12:
             pb = 4
             return pb
-        elif self.Level >=13 and self.Level <=16:
+        elif self.level >=13 and self.level <=16:
             pb = 5
             return pb
-        elif self.Level >=17 and self.Level <=20:
+        elif self.level >=17 and self.level <=20:
             pb = 6
-            return 6
-        self.proficiency_bonus = pb       
+            return pb
+        self.proficiency_bonus = pb
+        
+c1 = Character("Johnathan", 13)
+print("Proficiency bonus:",c1.proficiency())
