da6e3737bf44205ddeaf05a41a64e7bceeab6f96
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-05-12 23:32:26 -0300

    Add files via upload

--- new.py
+++ new.py
@@ -10,16 +10,31 @@ import time as t
 import d6_roller
 
 class Character:
-    def __init__(self, name, level:int,
-                 ability_scores={}, ability_mods={},
+    """Create a character in the Dungeons & Dragons 5th Edition format.
+    """
+    def __init__(self, name, level:int, random=False,
+                 ability_scores={}, _ability_mods={},
                  proficiency_bonus=0):
+        """Sets the initial state for a new character.
+        """
         name = input("Character name: ")
-        self.name = name
+        self._name = name
         level = int(input("Character level: "))
         self.level = level
+        random = input("Generate random? Reply Y for yes or N for no. ").strip().lower()
+        self.random = random
+        if "y" in self.random:
+            self.random = True
+        elif "n" in self.random:
+            self.random = False
         return
        
     def abilities(self):
+        """Allows the player to set their character's ability scores manually,
+        then defines the respective ability modifiers according to those values.
+        The way the ability modifiers are calculated is as described in the Player's Handbook:
+        (ability score - 10), divided by 2 and rounded down.
+        """
         print("Enter your ability scores:")
         t.sleep(1.2)
         self.ability_scores = {}
@@ -33,22 +48,55 @@ class Character:
             self.ability_scores["Wisdom"] = int(input("Wisdom: "))
             self.ability_scores["Charisma"] = int(input("Charisma: "))
 
-        self.ability_mods = {}
+        self._ability_mods = {}
         modifier = True
         while modifier:
             modifier = False
-            self.ability_mods["Strength"] = (self.ability_scores.get("Strength") - 10)//2
-            self.ability_mods["Dexterity"] = (self.ability_scores.get("Dexterity") - 10)//2
-            self.ability_mods["Constitution"] = (self.ability_scores.get("Constitution") - 10)//2
-            self.ability_mods["Intelligence"] = (self.ability_scores.get("Intelligence") - 10)//2
-            self.ability_mods["Wisdom"] = (self.ability_scores.get("Wisdom") - 10)//2
-            self.ability_mods["Charisma"] = (self.ability_scores.get("Charisma") - 10)//2
+            self._ability_mods["Strength"] = (self.ability_scores.get("Strength") - 10)//2
+            self._ability_mods["Dexterity"] = (self.ability_scores.get("Dexterity") - 10)//2
+            self._ability_mods["Constitution"] = (self.ability_scores.get("Constitution") - 10)//2
+            self._ability_mods["Intelligence"] = (self.ability_scores.get("Intelligence") - 10)//2
+            self._ability_mods["Wisdom"] = (self.ability_scores.get("Wisdom") - 10)//2
+            self._ability_mods["Charisma"] = (self.ability_scores.get("Charisma") - 10)//2
         
         print("Ability scores:",self.ability_scores)
-        print("Ability modifiers:",self.ability_mods)
+        print("Ability modifiers:",self._ability_mods)
+        return
+    
+    def random_scores(self):
+        """Randomizes your character's ability scores.
+        Hope you're feeling lucky!
+        """
+        self.ability_scores = {}
+        score = True
+        while score:
+            self.ability_scores["Strength"] = d6_roller.ability_rolls()
+            self.ability_scores["Dexterity"] = d6_roller.ability_rolls()
+            self.ability_scores["Constitution"] = d6_roller.ability_rolls()
+            self.ability_scores["Intelligence"] = d6_roller.ability_rolls()
+            self.ability_scores["Wisdom"] = d6_roller.ability_rolls()
+            self.ability_scores["Charisma"] = d6_roller.ability_rolls()
+            score = False
+            
+        self._ability_mods = {}
+        modifier = True
+        while modifier:
+            self._ability_mods["Strength"] = (self.ability_scores.get("Strength") - 10)//2
+            self._ability_mods["Dexterity"] = (self.ability_scores.get("Dexterity") - 10)//2
+            self._ability_mods["Constitution"] = (self.ability_scores.get("Constitution") - 10)//2
+            self._ability_mods["Intelligence"] = (self.ability_scores.get("Intelligence") - 10)//2
+            self._ability_mods["Wisdom"] = (self.ability_scores.get("Wisdom") - 10)//2
+            self._ability_mods["Charisma"] = (self.ability_scores.get("Charisma") - 10)//2
+            modifier = False
+        
+        t.sleep(1.5)
+        print("Ability scores:",self.ability_scores)
+        print("Ability modifiers:",self._ability_mods)
         return
     
     def proficiency(self):
+        """Defines your character's proficiency bonus based on their level.
+        """
         if self.level <=4:
             pb = 2
             return pb
@@ -70,12 +118,20 @@ class Character:
 #     def __init__(self, cls):
         
 c1 = Character("name", 0)
-c1.abilities()
+if c1.random == True:
+    c1.random_scores()
+else:
+    c1.abilities()
+    
 c2 = Character("name", 0)
-c2.abilities()
+if c2.random == True:
+    c2.random_scores()
+else:
+    c2.abilities()
 
 if c2.ability_scores.get("Intelligence") < 10:
-    print(f"{c2.name} is not very smart...")
+    print(f"{c2._name} is not very smart...")
+    
 print("Proficiency bonus:",c1.proficiency())
-print(c1.name,"is ready to rock")
-# print(c1.ability_mods.get("Strength"))
+print(c1._name,"is ready to rock")
+# print(c1._ability_mods.get("Strength"))
